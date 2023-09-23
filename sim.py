ee12bacbc9323703a470275b54e03741c3ec1b50
James Roberts <JRoberts86@gmail.com>
2023-09-23 22:21:30 +0100

    Optimized init condition to be list instead of array

--- sim.py
+++ sim.py
@@ -2,14 +2,11 @@
 
 import engine
 
-import numpy as np
-import matplotlib.pyplot as plt
-
 def main():
     HAB = engine.Flight()
     solver = engine.ODESolver(f=HAB.balloon_dynamics)
 
-    x0 = np.array([0.1, 0.0]) # h=0m, v=0m/s
+    x0 = [0.1, 0.0] # h=0m, v=0m/s
     dt = 0.1  # Time-step (0.001 = ms)
 
     solver.reset(x0, t_start=0.0); X, T = solver.compute(dt, "EULER") # Simulate
