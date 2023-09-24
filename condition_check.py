6991b249c89045d8ed9cc60a10ecd022bed24499
James Roberts <JRoberts86@gmail.com>
2023-09-25 00:27:32 +0100

    Add location and formatting

--- condition_check.py
+++ condition_check.py
@@ -18,9 +18,13 @@ args = parser.parse_args(sys.argv[1:])
 data = echo(f"https://api.weather.gov/points/{args.lat},{args.long}")
 forecast = echo(data["properties"]["forecast"])
 
+city = data['properties']['relativeLocation']['properties']['city']
+state = data['properties']['relativeLocation']['properties']['state']
+
 temp = forecast["properties"]["periods"][0]["temperature"]
 wind_speed = forecast["properties"]["periods"][0]["windSpeed"]
 wind_dir = forecast["properties"]["periods"][0]["windDirection"]
 
-print(f"Latitude: {args.lat} | Longitude: {args.long}")
+print(f"Lat|Long: [{args.lat}, {args.long}]")
+print(f"City: {city} | State: {state}")
 print(f"Temperature: {temp} | Wind Speed: {wind_speed} | Wind Direction: {wind_dir}")
