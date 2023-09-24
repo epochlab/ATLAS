14cf2fd10fde637003954fc470e7929202b2aac2
James Roberts <JRoberts86@gmail.com>
2023-09-25 00:52:18 +0100

    Add state_time

--- condition_check.py
+++ condition_check.py
@@ -5,7 +5,7 @@ import argparse, sys, requests
 def echo(url):
     response = requests.get(url)
     if response.status_code == 200:
-        return response.json()
+        return response.json()['properties']
     else:
         print(f"Request failed with status code {response.status_code}")
         exit()
@@ -16,15 +16,17 @@ parser.add_argument('-long', type=float, default=-91.1871)
 args = parser.parse_args(sys.argv[1:])
 
 data = echo(f"https://api.weather.gov/points/{args.lat},{args.long}")
-forecast = echo(data["properties"]["forecast"])
+forecast = echo(data["forecast"])
 
-city = data['properties']['relativeLocation']['properties']['city']
-state = data['properties']['relativeLocation']['properties']['state']
-
-temp = forecast["properties"]["periods"][0]["temperature"]
-wind_speed = forecast["properties"]["periods"][0]["windSpeed"]
-wind_dir = forecast["properties"]["periods"][0]["windDirection"]
-
+city = data['relativeLocation']['properties']['city']
+state = data['relativeLocation']['properties']['state']
 print(f"Lat|Long: [{args.lat}, {args.long}]")
 print(f"City: {city} | State: {state}")
+
+temp = forecast["periods"][0]["temperature"]
+wind_speed = forecast["periods"][0]["windSpeed"]
+wind_dir = forecast["periods"][0]["windDirection"]
+start_time = forecast["periods"][0]["startTime"]
+
+print(start_time)
 print(f"Temperature: {temp} | Wind Speed: {wind_speed} | Wind Direction: {wind_dir}")
